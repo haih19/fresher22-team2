bb835b5f9d98fa46eeba9df3a3cd18eb1e5b1258
haih19 <haih19c@gmail.com>
2023-02-01 16:21:36 +0700

    feature/percentCircle is undergoing

--- src/components/percentCirlce/index.jsx
+++ src/components/percentCirlce/index.jsx
@@ -1,16 +1,24 @@
 import './percentCircle.scss'
+import PropTypes from 'prop-types'
 
-function PercentCircle() {
+function PercentCircle(props) {
    return (
       <div className="percent-circle">
-         <span className="percent-circle__b">90%</span>
-         <div className="percent-circle__a">
-            aafafagafgaga
-            <div className="percent-circle__a__bar">bar</div>
-            <div className="fill"></div>
+         <span className="percent-circle__num">
+            {props.number}
+            <span>%</span>
+         </span>
+         <div className="percent-circle__slice">
+            <div className="percent-circle__slice__bar">bar</div>
+            <div className="percent-circle__slice__fill"></div>
          </div>
       </div>
    )
+}
+
+PercentCircle.propTypes = {
+   number: PropTypes.string,
+   color: PropTypes.string,
 }
 
 export default PercentCircle
