fde3df6f93f5e24cc83a3c0d02a6d59b8ed66582
haih19 <haih19c@gmail.com>
2023-02-01 15:16:13 +0700

    update: add fontawesome and add fontawesome

---
+++ src/components/demoFontAwesome/index.jsx
@@ -0,0 +1,12 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons'
+
+function Demo() {
+   return (
+      <div>
+         <FontAwesomeIcon icon={faCheckCircle} />
+      </div>
+   )
+}
+
+export default Demo
