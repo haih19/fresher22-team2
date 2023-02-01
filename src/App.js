cfb72530bf891949d63b028450e99d0a5ee2d77b
haih19 <haih19c@gmail.com>
2023-02-01 14:53:19 +0700

    update: scss bem

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomeLayout from './layout/homeLayout'
 import CategoryLayout from './layout/categoryLayout'
 import {useEffect} from 'react'
 import guestService from './services/user.service'
+import PercentCircle from './components/percentCirlce'
+
 import './App.scss'
 function App() {
    useEffect(() => {
@@ -18,25 +20,26 @@ function App() {
       getMovieList()
    }, [])
    return (
-      <BrowserRouter>
-         <Routes>
-            {routeList.map((route, index) => {
-               let Layout = route.layout === 'home' ? HomeLayout : CategoryLayout
+      // <BrowserRouter>
+      //    <Routes>
+      //       {routeList.map((route, index) => {
+      //          let Layout = route.layout === 'home' ? HomeLayout : CategoryLayout
 
-               return (
-                  <Route
-                     key={index}
-                     path={route.path}
-                     element={
-                        <Layout>
-                           <route.component />
-                        </Layout>
-                     }
-                  />
-               )
-            })}
-         </Routes>
-      </BrowserRouter>
+      //          return (
+      //             <Route
+      //                key={index}
+      //                path={route.path}
+      //                element={
+      //                   <Layout>
+      //                      <route.component />
+      //                   </Layout>
+      //                }
+      //             />
+      //          )
+      //       })}
+      //    </Routes>
+      // </BrowserRouter>
+      <PercentCircle />
    )
 }
 
--- src/components/percentCirlce/index.jsx
+++ src/components/percentCirlce/index.jsx
@@ -3,9 +3,10 @@ import './percentCircle.scss'
 function PercentCircle() {
    return (
       <div className="percent-circle">
-         <span>90%</span>
-         <div className="percent-circle__">
-            <div className="bar"></div>
+         <span className="percent-circle__b">90%</span>
+         <div className="percent-circle__a">
+            aafafagafgaga
+            <div className="percent-circle__a__bar">bar</div>
             <div className="fill"></div>
          </div>
       </div>
