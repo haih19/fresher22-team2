caaa25084755b9da7318c48ce39353036aa9fc00
Leeminchun <lchung402@gmail.com>
2023-02-10 15:36:58 +0700

    done home

--- src/components/movieHome/contentHome/listTrendingHome/cardTrendHomeList.jsx
+++ src/components/movieHome/contentHome/listTrendingHome/cardTrendHomeList.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react'
 import CardHome from '../cardHome/cardHome'
 import '../contentHome.scss'
 import tmdbService from '../../../../services/tmdb.service'
-import {RequestHome} from '../requsetHome'
+import { RequestHome } from '../requsetHome'
 import Blur from '../blurHome'
-const CardHomeList = ({fetch}) => {
+import { Link } from 'react-router-dom'
+const CardHomeList = ({ fetch }) => {
 
   const [dataCardTrending, setDataCardTrending] = useState([])
   useEffect(() => {
@@ -12,7 +13,7 @@ const CardHomeList = ({fetch}) => {
       try {
         const res = await tmdbService.getTrendingList('movie', { time_window: RequestHome[fetch] })
         setDataCardTrending(res.results);
-       
+
       }
       catch (e) {
         console.log(e);
@@ -20,18 +21,20 @@ const CardHomeList = ({fetch}) => {
     }
     fechData()
   }, [fetch])
-  
+
   return (
     <div className='wrap-cards'>
       {dataCardTrending.map((item, index) => {
         return (
-          <CardHome key={index} imgCard={tmdbService.posterPath(item.poster_path)}
-          title={item.title}
-          release_date={item.release_date} 
-          percent = {item.vote_average}/>
+          <Link to={`/movie/${item.id}`}>
+            <CardHome key={index} imgCard={tmdbService.posterPath(item.poster_path)}
+              title={item.title}
+              release_date={item.release_date}
+              percent={item.vote_average} />
+          </Link>
         )
       })}
-      <Blur/>
+      <Blur />
     </div>
   )
 }
